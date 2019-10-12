d52f52c745727beca1cc1bb8b3d45f66c8776c91
cybercoder-naj <cybercoder.nishant@gmail.com>
2019-10-12 11:53:18 +0530

    Initial Commit

---
+++ src/main/java/com/nishant/complexcalculator/ComplexCalculator.java
@@ -0,0 +1,219 @@
+package com.nishant.complexcalculator;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This class can compute mathematical operations based on an expression.
+ * The expression is stored in {@code String function}.
+ *
+ * @author Nishant
+ * @version 1.0
+ * @since 18-09-2019
+ */
+public class ComplexCalculator {
+    private String function;
+
+    /**
+     * Instantiate the object with an expression.
+     *
+     * The function can perform the following operations:-
+     * <ul>
+     *     <li>Exponent (^)</li>
+     *     <li>Division (/)</li>
+     *     <li>Multiplication (*)</li>
+     *     <li>Addition (+)</li>
+     *     <li>Subtraction (-)</li>
+     * </ul>
+     * If you like to involve variables in the function, you are most welcome to do so.
+     * If there are any variables, you must map them to double values in a {@code Map<Character, Double>} collection.
+     * The method will replace the variables with your value and compute the given task.
+     * <br><br>You can use <i>pi</i> in the middle of the function to use the mathematical value of {@code Math.PI}.<br>
+     * You can also you <i>E</i> to denote exponent. <code>For example, 3.0E10 = 3.0 * 10<sup>10</sup></code>.
+     * The rules of using <i>E</i> will coincide with that of {@code Double}:-
+     * <ul>
+     *     <li>The digits after E must not be fractional</li>
+     *     <li>The digits before E must have a integral and a fractional part.</li>
+     * </ul>
+     *
+     * @param function given expression.
+     */
+    public ComplexCalculator(String function) {
+        this.function = function;
+    }
+
+    /**
+     * @return the expression
+     */
+    public String getFunction() {
+        return function;
+    }
+
+    /**
+     * Modify the expression to replace the old one.
+     *
+     * @param function new expression.
+     */
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    /**
+     * Sets the new expression, which is of another <i>ComplexCalculator</i> instance.
+     *
+     * @param other instance to set the new expression
+     */
+    public void setFunctionOf(ComplexCalculator other) {
+        setFunction(other.function);
+    }
+
+    /**
+     * Returns the answer of the given expression.
+     * <br>The map collection has a character key and a double value. Each Character in the key will be replaced by its
+     * respective double value before computer the expression.
+     * <br><br>
+     * The following with result in Exceptions:-
+     * <ul>
+     * <li>replacing E as a variable.</li>
+     * <li>using unexpected letters.</li>
+     * <li>missing round brackets.</li>
+     * <li>using brackets other than round brackets</li>
+     * <li>use of curly or square brackets.</li>
+     * <li>{@code variableMap} is null.</li>
+     * <li>{@code function} is null or empty.</li>
+     * <li>If other unknown computing error occurs.</li>
+     * <li>If there is any error in the code.</li>
+     * </ul>
+     *
+     * @see Double
+     * @see Map
+     * @param variableMap characters in key will be replaced by its respective double value.
+     * @return the solution to the given expression and value of variables.
+     */
+    public double compute(Map<Character, Double> variableMap) {
+        try {
+            if (this.getFunction() == null) throw new NullPointerException("function is null");
+            if (this.getFunction().length() == 0) throw new IllegalStateException("Function is empty");
+            if (variableMap == null) throw new NullPointerException("variableMap is null");
+
+            String function = "(" + this.getFunction() + ")";
+            function = function.replaceAll(" ", "");
+            function = function.replaceAll("pi", String.valueOf(Math.PI));
+            for (Entry<Character, Double> entry : variableMap.entrySet()) {
+                if (entry.getKey() == 'E') throw new IllegalArgumentException("Cannot replace E as a variable.");
+                function = function.replaceAll(entry.getKey().toString(), entry.getValue().toString());
+            }
+            Matcher matcher = Pattern.compile("[a-zA-DF-Z]").matcher(function);
+            if (matcher.find()) throw new IllegalArgumentException("Unexpected letter: " + matcher.group());
+            if (count(function, '(') > count(function, ')'))
+                throw new IllegalArgumentException("Missing '('");
+            if (count(function, '(') < count(function, ')'))
+                throw new IllegalArgumentException("Missing ')'");
+
+            Matcher matcher1 = Pattern.compile("[(](-?\\d+(\\.\\d+(E-?\\d+)?)?[\\^/*+\\-])+-?\\d+(\\.\\d+(E-?\\d+)?)?[)]").matcher(function);
+            Matcher matcher2 = Pattern.compile("[(]-?\\d+(\\.\\d+(E-?\\d+)?)?[)]").matcher(function);
+            Matcher matcher3 = Pattern.compile("[(]?-{2}").matcher(function);
+            boolean match1 = matcher1.find();
+            boolean match2 = matcher2.find();
+            boolean match3 = matcher3.find();
+            while (match1 || match2 || match3) {
+                String group;
+                if (match1) {
+                    group = matcher1.group();
+                    if (group.indexOf('^') != -1) function = performOperation('^', group, function);
+                    else if (group.indexOf('/') != -1) function = performOperation('/', group, function);
+                    else if (group.indexOf('*') != -1) function = performOperation('*', group, function);
+                    else if (group.indexOf('+') != -1) function = performOperation('+', group, function);
+                    else if (group.indexOf('-') != -1 && group.charAt(group.indexOf('-') - 1) != '(')
+                        function = performOperation('-', group, function);
+                    else if (group.lastIndexOf('-') != 1) function = performOperation('-', group, function);
+
+                }
+
+                matcher2.reset(function);
+                match2 = matcher2.find();
+                if (match2) {
+                    group = matcher2.group();
+                    function = function.replace(group, group.substring(1, group.length() - 1));
+                }
+
+                matcher3.reset(function);
+                match3 = matcher3.find();
+                if (match3) {
+                    group = matcher3.group();
+                    function = function.replace(group, "+");
+                }
+
+                matcher1.reset(function);
+                matcher2.reset(function);
+                matcher3.reset(function);
+                match1 = matcher1.find();
+                match2 = matcher2.find();
+                match3 = matcher3.find();
+            }
+
+            try {
+                return Double.parseDouble(function);
+            } catch (NumberFormatException ex) {
+                throw new RuntimeException("Unable to compute the given string.");
+            }
+
+
+        } catch (Exception ex) {
+            throw new RuntimeException("Found unknown error.");
+        }
+    }
+
+    private int count(String function, char c) {
+        int f = 0;
+
+        for (int i = 0; i < function.length(); i++) {
+            if (function.charAt(i) == c) f++;
+        }
+
+        return f;
+    }
+
+    private String performOperation(char operator, String group, String function) {
+        StringBuilder num1 = new StringBuilder();
+        StringBuilder num2 = new StringBuilder();
+        int i;
+        if (operator != '-') {
+            for (i = group.indexOf(operator) - 1; group.charAt(i) == 'E' || Character.isDigit(group.charAt(i)) || group.charAt(i) == '.' || group.charAt(i) == '-' && (group.charAt(i - 1) == '(' || group.charAt(i - 1) == 'E'); i--) {
+                num1.insert(0, group.charAt(i));
+            }
+
+            for (i = group.indexOf(operator) + 1; group.charAt(i) == 'E' || Character.isDigit(group.charAt(i)) || group.charAt(i) == '.' || group.charAt(i) == '-' && (i == group.indexOf(operator) + 1 || group.charAt(i - 1) == 'E'); i++) {
+                num2.append(group.charAt(i));
+            }
+        } else {
+            for (i = group.lastIndexOf(operator) - 1; group.charAt(i) == 'E' || Character.isDigit(group.charAt(i)) || group.charAt(i) == '.' || group.charAt(i) == '-' && (group.charAt(i - 1) == '(' || group.charAt(i - 1) == 'E'); i--) {
+                num1.insert(0, group.charAt(i));
+            }
+
+            for (i = group.lastIndexOf(operator) + 1; group.charAt(i) == 'E' || Character.isDigit(group.charAt(i)) || group.charAt(i) == '.' || group.charAt(i) == '-' && (i == group.lastIndexOf(operator) + 1 || group.charAt(i - 1) == 'E'); i++) {
+                num2.append(group.charAt(i));
+            }
+        }
+
+        double n1 = Double.parseDouble(num1.toString());
+        double n2 = Double.parseDouble(num2.toString());
+        String toReplace = num1.toString() + operator + num2.toString();
+        switch (operator) {
+            case '*':
+                return function.replace(toReplace, String.valueOf(n1 * n2));
+            case '+':
+                return function.replace(toReplace, String.valueOf(n1 + n2));
+            case '-':
+                return function.replace(toReplace, String.valueOf(n1 - n2));
+            case '/':
+                return function.replace(toReplace, String.valueOf(n1 / n2));
+            case '^':
+                return function.replace(toReplace, String.valueOf(Math.pow(n1, n2)));
+            default:
+                throw new IllegalStateException("Unexpected value: " + operator);
+        }
+    }
+}
